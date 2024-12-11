ec02eda0ffdb5565d0224116a126977115b1534b
olajirestephen <horlharmighty2000@gmail.com>
2024-12-11 13:17:36 -0600

    cloud

--- frontend/src/component/JournalCrad.jsx
+++ frontend/src/component/JournalCrad.jsx
@@ -3,20 +3,27 @@ import { Download, Edit } from "lucide-react";
 import styles from "../style/JournalCrad.module.css"; // Import the CSS module
 
 const JournalCrad = ({ journal }) => {
+  
   return (
     <div className={styles.cardContainer}>
       <div className={styles.cardImage}>
-        <a href={journal.front_page} >
-          <img src={journal.front_page} alt={journal.project_name} />
+        <a href={journal?.front_page}>
+          <img
+            src={journal?.front_page}
+            alt={journal?.project_name || "Journal Image"}
+          />
         </a>
       </div>
       <div className={styles.cardContent}>
-        <p className={styles.cardDescription}>{journal.faculty}</p>
-        <h1 className={styles.cardTitle}>{journal.project_name}</h1>
-        <p className={styles.cardDescription}>{journal.department}</p>
-
+        <p className={styles.cardDescription}>{journal?.faculty}</p>
+        {journal?.project_name > 50 ? (
+          <h1 className={styles.cardTitle}>{journal.project_name.slice(0, 30)} ...</h1>
+        ): (
+          <h1 className={styles.cardTitle}>{journal.project_name}</h1>
+        )}
+        <p className={styles.cardDescription}>{journal?.department}</p>
         <div className={styles.cardActions}>
-          <a href={journal.project_file} download>
+          <a href={journal?.project_file} download>
             <Download size={24} />
           </a>
           <Edit size={24} />

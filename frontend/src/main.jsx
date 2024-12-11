e9d8e138c4aab6e879360b427c5c297d1ca94970
olajirestephen <horlharmighty2000@gmail.com>
2024-12-11 08:15:07 -0600

    component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import "../src/Css/style.css"
+import "../src/style/style.css"
 
 
 createRoot(document.getElementById('root')).render(
